f89928dc191f7c68da21e4e4c3a8abe861f9aaca
shubhamp2511 <shubham.darshi2511@gmail.com>
2025-03-18 19:34:03 +0530

    Added components and styles folder
    1. components/Card.js- individual card component
    2. components/CardModal.js:-Modal component for card details.
    3. components/Header.js :- header or navbar
    4. components/Footer.js:- footer
    5. styles/index.css:-main styles

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+      <header className="w-full p-4 bg-gray-800 text-white text-center shadow-md">
+        <h1 className="text-2xl font-bold">Cred Club Cards</h1>
+      </header>
+    );
+  }
+  
